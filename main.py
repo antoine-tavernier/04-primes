e2f20599044cf35a8190b371f30a977d41b71d54
antoine-tavernier <antoine.tavernier@edu.esiee.fr>
2024-11-14 10:28:25 +0000

    lecon 4 fonctions et modules, exercice nombre premier

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from math import sqrt
 
 #### Fonction secondaire
 
-
-def isprime(p):
-
-    # votre code ici
-
-    pass
+def isprime(p): 
+    """Retourne vrai si le nombre est un npmbre premier""" 
+    if p in (0, 1): 
+        return False 
+    for i in range(2, int(sqrt(p)) + 1): 
+        if (p % i == 0): 
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    """Fonction permettant d'obtenir puis d'afficher tous les nombres premier jusqu'à 100""" 
     # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
